6d999155a6dd5e3e356df300d134de7ee8ffe1a4
Astrid Boronda Heidinger <astrid.boronda@gmail.com>
2025-06-14 16:18:04 -0500

    feat: add enviroments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   //serverBasePath: 'http://localhost:8080/api/v1'
-  serverBasePath: 'https://medsystem-backend-production.up.railway.app/api/v1'
+  //serverBasePath: 'https://medsystem-backend-production.up.railway.app/api/v1'
+  serverBasePath: 'https://backend-uy6n.onrender.com/swagger-ui/index.html/api/v1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   //serverBasePath: 'http://localhost:8080/api/v1'
-  serverBasePath: 'https://medsystem-backend-production.up.railway.app/api/v1'
+  //serverBasePath: 'https://medsystem-backend-production.up.railway.app/api/v1'
+  serverBasePath: 'https://backend-uy6n.onrender.com/swagger-ui/index.html/api/v1',
 };
